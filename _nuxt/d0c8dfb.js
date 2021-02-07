069f861d6e1ab37975523d550c25774c1ca2272a
Evgeny <evg.develop@gmail.com>
2021-02-07 20:37:30 +0500

    Updates

---
+++ _nuxt/d0c8dfb.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{244:function(n,t,e){"use strict";e.r(t);var c={},l=e(15),component=Object(l.a)(c,(function(){var n=this.$createElement;return(this._self._c||n)("div",{staticClass:"container"},[this._v("\n    My Handbook\n")])}),[],!1,null,null,null);t.default=component.exports}}]);
